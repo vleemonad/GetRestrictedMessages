2b0d09096ba39cde8d16d2d379df23683bf1afc9
vleemonad <leemonad@yahoo.com>
2026-01-22 17:55:00 +0200

    Update sessiongen.py

--- sessiongen.py
+++ sessiongen.py
@@ -1,8 +1,17 @@
 from telethon import TelegramClient
 from telethon.sessions import StringSession
+import asyncio
 
-API_ID = int(input("Enter API ID: "))
-API_HASH = input("Enter API HASH: ")
+API_ID = int(input("Enter API_ID: ").strip())
+API_HASH = input("Enter API_HASH: ").strip()
 
-with TelegramClient(StringSession(), API_ID, API_HASH) as client:
-    print("Here is your session string:\n" + client.session.save())
+
+async def main():
+    async with TelegramClient(StringSession(), API_ID, API_HASH) as client:
+        print("\nYour SESSION string:")
+        print(client.session.save())
+        print("\nCopy this whole line and paste it into your SESSION env variable.")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
